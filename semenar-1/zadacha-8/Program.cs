4bff36114f9066e4d70b07cc198d76ae28af35db
borges <ale22071989@yandex.ru>
2022-08-08 17:59:45 +0300

    semenar-1

---
+++ semenar-1/zadacha-8/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int i =0;
+while (i<=num1)
+{
+    Console.Write($"{i} ") ;
+    i+=2;
+}
