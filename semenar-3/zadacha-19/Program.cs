3ae461f7800d3e157fbcb4431e0b84c2e5aeecbf
borges <ale22071989@yandex.ru>
2022-08-15 18:11:29 +0300

    semenar-3

---
+++ semenar-3/zadacha-19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, 
+// которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+ int  n, reverse=0, rem;           
+       Console.Write("Enter a number: ");      
+       n= int.Parse(Console.ReadLine()); 
+    int f=n;      
+       while(n!=0)      
+       {      
+        rem=n%10;        
+        reverse=reverse*10+rem;      
+        n/=10;      
+       }    
+       int s = reverse;
+if (f==s)
+{
+Console.WriteLine("Полиндром");   
+ }
+else
+{
+    Console.WriteLine("Не полиндром"); 
+}       
+// работает не только для 5значного числа 
+    
+    
+  
---
+++ semenar-3/zadacha-23/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите значение!");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+if (n>0)
+{
+    while (i<=n)
+    {
+        int q = i*i*i;
+        Console.WriteLine($"{i}|{q}");
+        i++;
+    }
+    
+}
+else
+    {
+        Console.WriteLine($"Число должно быть больше 0");
+    }
