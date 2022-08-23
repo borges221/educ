985e33a9fd180deaa0107ee86b3f33b9b68fe61b
borges <ale22071989@yandex.ru>
2022-08-23 19:38:39 +0300

    semenar-6

---
+++ semenar-6/zadacha-41/Program.cs
@@ -0,0 +1,27 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите колличество чисел  ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] arraynum = new int[m];
+
+void ImputElementArray(int m)
+{
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите {i+1} число ");
+        arraynum[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int CountNotMinusZero(int[] arraynum)
+{
+    int count = 0;
+    for (int i = 0; i < arraynum.Length; i++)
+    {
+      if(arraynum[i]>0) count+=1;    
+    }
+    return count;
+}
+ImputElementArray(m);
+Console.WriteLine($"Ввдено чисел больше 0 ---> {CountNotMinusZero(arraynum)}");
---
+++ semenar-6/zadacha-43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.WriteLine("Пересекутся ли прямы заданые уровненями y = k1 * x + b1, y = k2 * x + b2??? ");
+Console.WriteLine("Ввелите значение b1:");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите значение k1:");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите значение b2:");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ввелите значение k2:");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+void MeetingPoint(double b1, double k1, double b2, double k2)
+{
+    double[] dot = new double[2];
+    dot[0] = (b2 - b1) / (k1 - k2);
+    dot[1] = k1 * dot[0] + b1;
+
+    if ((k1 * dot[0] + b1) == (k2 * dot[0] + b2)) 
+    {
+
+
+        Console.Write("Пряммые пересекаются в точке: ");
+        Console.Write($"[{String.Join(", ", dot)}]");
+    }
+    else
+    {
+    Console.WriteLine("Прямые не пересекаются.");
+    }
+}
+MeetingPoint(b1,k1,b2,k2);
