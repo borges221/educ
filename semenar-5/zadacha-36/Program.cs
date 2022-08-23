ffa75a9b773831d24cf21587750573c5eb499a84
borges <ale22071989@yandex.ru>
2022-08-23 19:30:03 +0300

    semenar-5

---
+++ semenar-5/zadacha-36/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+// стоящих на нечётных позициях (индексах).
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+    }
+
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i<array.Length-1)Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.Write("]");
+}
+
+int SumOdd(int[] array)
+{
+  int sum = 0;
+for(int i = 1; i < array.Length; i+=2)
+{
+    sum += array[i];
+}
+return sum;
+}
+
+int[] array  = CreateArrayRndInt(10000, -10000, 10000); 
+PrintArray(array);
+Console.WriteLine($"Сумм чисел на нечетных индексах массива = {SumOdd(array)}");
+
